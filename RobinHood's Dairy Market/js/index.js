a702e5b91deedadbb5b191a5e12250a2be801e3d
Amar Mahmoud <74137093+Amar-Mahmoud@users.noreply.github.com>
2022-01-23 17:57:50 -0500

    Add files via upload

---
+++ RobinHood's Dairy Market/js/index.js
@@ -0,0 +1,9 @@
+ window.toggleLeft = function() {
+     const element = document.getElementById("offcanvas-left");
+     element.classList.toggle("hide");
+}
+
+window.toggleRight = function() {
+    const element = document.getElementById("offcanvas-right");
+    element.classList.toggle("hide");
+}
